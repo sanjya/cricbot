4138778db98c82378a7bc4abb949d6f1eab647f7
sanjya <sanjayanuwanaj@gmail.com>
2016-07-03 16:32:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ echo "done";
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    switch (messageText) {
+    switch ($messageText) {
       case 'image':
         sendWelcomeMessage($senderID);
         break;
