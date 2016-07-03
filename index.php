3bd98ea9ffdd2e75af87f9662bc39569835efc31
sanjya <sanjayanuwanaj@gmail.com>
2016-07-03 10:16:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,4 +18,66 @@ echo $challenge;
 }
 
 
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+if(preg_match('[time|current time|now]', strtolower($message))) {
+ 
+    // Make request to Time API
+    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+    if($result != '') {
+        $message_to_reply = $result;
+    }
+} else {
+    $message_to_reply = 'Huh! what do you mean?';
+}
+print $message_to_reply;
+
+
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+ 
+ 
+//Initiate cURL.
+$ch = curl_init($url);
+ 
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    },
+    "message":{
+        "text":"'.$message_to_reply.'"
+    }
+}';
+ 
+//Encode the array into JSON.
+$jsonDataEncoded = json_encode($jsonData);
+ 
+$data = array("recipient" => array("id"=>"$sender"), "message" => array("text"=>"hi hi hi"));                                                                    
+$data_string = json_encode($data);                                                                                   
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);                                                                                                                   
+
+$result = curl_exec($ch);
+
+
+ 
+//Execute the request
+/*if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    $result = curl_exec($ch);
+
+echo "done";
+}*/
+
+
 ?>
