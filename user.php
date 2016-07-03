ff935f37bb9eb71e49a21747d1c302607ec1fa7e
sanjya <sanjayanuwanaj@gmail.com>
2016-07-03 16:52:30 +0530

    Update user.php

--- user.php
+++ user.php
@@ -1,7 +1,8 @@
 <?php
 $recipientId="1084524121623966";
-
-$input=  json_decode(file_get_contents("https://graph.facebook.com/v2.6/".$recipientId."?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD"));
+$url="https://graph.facebook.com/v2.6/".$recipientId."?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD";
+echo $url;
+$input=  json_decode(file_get_contents());
     $messageText="Hi ".$input['first_name'];
     echo $messageText; 
     //sendTextMessage($recipientId, $messageText);
