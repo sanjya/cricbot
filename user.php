0eadb4369fce8005c5d75b533be3e5d93b0e8944
sanjya <sanjayanuwanaj@gmail.com>
2016-07-08 11:48:31 +0530

    Update user.php

--- user.php
+++ user.php
@@ -1,29 +1,15 @@
 <?php
 $messageText="typing_on";
 $recipientId="1084524121623966";
-  sendImage($recipientId);
+  sendTextMessage($recipientId,"blala");
   
   print "sent";
 
-  /*$data = array("recipient" => array("id"=>$recipientId), 
-                     "sender_action"=>$messageText);                                                                    
-    $data_string = json_encode($data);  
-
-    callSendAPI($data_string);
+  
 
 
 
-/*$url="https://graph.facebook.com/v2.6/$recipientId?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD";
-//echo $url;
-//echo "\n";
-$input=  json_decode(file_get_contents($url));
 
-//print_r($input);
-//print $input;
-    $messageText="Hi ".$input->last_name;
-    echo $messageText; 
-    echo $input['last_name'];
-    //sendTextMessage($recipientId, $messageText);*/
     
     
    function sendTextMessage($recipientId, $messageText) {
