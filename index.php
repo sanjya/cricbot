d4b3a05c27d202c15b180ce640b1ad3ecabcc589
sanjya <sanjayanuwanaj@gmail.com>
2016-07-09 19:28:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token="EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD";
+$access_token=getenv('access_token');//"EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD";
 $verify_token="my_bot";
 $hub_verify_token="null";
 
