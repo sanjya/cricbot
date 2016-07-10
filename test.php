d761c71812134b4ad24710f9fb3a690000944ad6
sanjya <sanjayanuwanaj@gmail.com>
2016-07-10 16:58:17 +0530

    Button Sending

--- test.php
+++ test.php
@@ -14,6 +14,9 @@ $messageData= array("recipient"=>"1084524121623966", "message"=>array("attachmen
 
 
 $data= json_encode($messageData);
+
+print json_decode($data);
+
 callSendAPI($data);
 
 print "sent";
