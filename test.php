589b54578cf405b304a9e2880839b0cbc88d914b
sanjya <sanjayanuwanaj@gmail.com>
2016-07-10 15:54:00 +0530

    Button Sending

--- test.php
+++ test.php
@@ -1,4 +1,33 @@
 <?php
 
 echo getenv('VERIFY_TOKEN');
+
+$button1= array("type"=>"post","title"=>"View Score","payload"=>"view_score_match_id:") ;
+$buttons= array($button);
+
+$payload=array("template_type"=>"button","text"=>"Match name","buttons"=>$buttons);
+
+$attachment=array("type"=>"template","payload"=>$payload);
+
+$messageData= array("recipient"=>"1084524121623966", "message"=>array("attachment"=>$attachment));
+
+
+$data= json_encode($messageData);
+callSendAPI($data);
+
+print "sent";
+
+function callSendAPI($data_string){
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);                                                                                                                   
+$result = curl_exec($ch);
+}
+
+
 ?>
