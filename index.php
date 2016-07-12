a80702fc3c3c8f073a10f93fd230b7a8fc0c9b97
sanjya <sanjayanuwanaj@gmail.com>
2016-07-12 08:13:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php
-
 require './ReceivedPostBack.php';
-
-
 $access_token=getenv('access_token');
 $verify_token="my_bot";
 $hub_verify_token="null";
-
-
 if(isset($_REQUEST['hub_challenge'])){
-
 $challenge= $_REQUEST['hub_challenge'];
 $hub_verify_token= $_REQUEST['hub_verify_token'];
-
 }
-
 if($hub_verify_token==$verify_token){
 echo $challenge;
-
 }
-
-
-
 $input = json_decode(file_get_contents('php://input'), true);
-
-
 // Make sure this is a page subscription
   if ($input['object'] == 'page') {
     // Iterate over each entry
@@ -35,8 +21,6 @@ $input = json_decode(file_get_contents('php://input'), true);
     foreach ($entries as $entry) {
         //1084524121623966
         $messages= $entry['messaging'];
-
-
         foreach ($messages as $message) {
             
             //need to check message event type, but this app is only subcribed to "message" event 
@@ -44,7 +28,6 @@ $input = json_decode(file_get_contents('php://input'), true);
             $event = end($keys);
             
             
-
             if($event=="message"){
               
               receivedMessage($message);
@@ -55,26 +38,16 @@ $input = json_decode(file_get_contents('php://input'), true);
               $postback->handle();
               
             }
-
             
         }
     }
   }
-
-
-
  
   
   
-
-
-
-
-
 /*
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
-
 if(preg_match('[time|current time|now]', strtolower($message))) {
  
     // Make request to Time API
@@ -87,8 +60,6 @@ if(preg_match('[time|current time|now]', strtolower($message))) {
     $message_to_reply = 'Huh! what do you mean?';
 }
 print $message_to_reply;
-
-
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
  
  
@@ -105,32 +76,22 @@ $jsonData = '{
     }
 }';
  
-
 }*/
-
-
-
-
  function receivedMessage($event){
    $senderID = $event['sender']['id'];
    $recipientID = $event['recipient']['id'];
    $timeOfMessage = $event['timestamp'];
    $message = $event['message'];
-
   /*console.log("Received message for user %d and page %d at %d with message:", 
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));*/
-
    $messageId = $message['mid'];
    $seq= $message['seq'];
-
   // You may get a text or attachment but not both
    $messageText = $message['text'];
    //$messageAttachments = $message['attachments'];
-
   
   if ($messageText) {
-
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
@@ -141,15 +102,12 @@ $jsonData = '{
       case "hi":
         sendWelcomeMessage($senderID);
         break;
-
      case 'button':
         sendButtonMessage($senderID);
         break;
-
       case 'generic':
         sendGenericMessage($senderID);
         break;
-
       case 'image':
         sendImage($senderID);
         break;
@@ -165,7 +123,6 @@ $jsonData = '{
         sendLiveMessage($senderID);
         break;
     
-
       default:
         $messageText= "Sorry for rebellion \n type help to suppress";
         sendTextMessage($senderID, $messageText);
@@ -175,11 +132,9 @@ $jsonData = '{
   }
   }
   
-
 function sendWelcomeMessage($recipientId){
     
-    
-    $input=  json_decode(file_get_contents("https://graph.facebook.com/v2.6/".$recipientId."?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD",true);
+    $input=  json_decode(file_get_contents("https://graph.facebook.com/v2.6/".$recipientId."?access_token=".$access_token),true);
     $messageText="Hi ".$input['first_name'];
     sendTextMessage($recipientId, $messageText);
 	//print $messageText;
@@ -200,7 +155,6 @@ function sendLiveMessage($recipientId){
 			$count++;
             $messageText="Match $count ".$entry['t2']." vs ".$entry['t1']."\n ";
             $id=$entry['id'];
-
              $button= array("type"=>"postback","title"=>"View Score match ","payload"=>"View Score $id") ;
             sendButtonMessage($recipientId,$entry['t2']." vs ".$entry['t1'],$button);
         }
@@ -211,38 +165,23 @@ function sendLiveMessage($recipientId){
     }
     
 } 
-
-
-
-
 function sendTextMessage($recipientId, $messageText) {
     
    
     $data = array("recipient" => array("id"=>$recipientId), 
                      "message" => array("text"=>$messageText));                                                                    
     $data_string = json_encode($data);  
-
     callSendAPI($data_string);
-
 }
-
-
 function sendAction($recipientId){
-
   $messageText="typing_on";
   
   $data = array("recipient" => array("id"=>$recipientId), 
                      "sender_action"=>$messageText);                                                                    
   $data_string = json_encode($data);  
-
   callSendAPI($data_string);
     
-
 }
-
-
-
-
 function sendImage($recipientId){
   //array("type"=>"image","payload"=>array("url"=>"https://petersapparel.com/img/shirt.png"))
   //$payload= array("url"=>"https://petersapparel.com/img/shirt.png");
@@ -257,28 +196,17 @@ function sendImage($recipientId){
   $data_string = json_encode($data);  
   callSendAPI($data_string);
 }
-
-
 function sendButtonMessage($recipientId,$payload_text,$button){
   
            
             $buttons= array($button);
-
             $payload=array("template_type"=>"button","text"=>$payload_text,"buttons"=>$buttons);
-
             $attachment=array("type"=>"template","payload"=>$payload);
-
             $messageData= array("recipient"=>array("id"=>"1084524121623966"), "message"=>array("attachment"=>$attachment));
-
-
             $data= json_encode($messageData);
-
             //print_r(json_decode($data));
-
             callSendAPI($data);
-
 }
-
 function callSendAPI($data_string){
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=EAAIiguQ4fcQBADgTCY78eONR4gly10IGjGaxNWIBLQziIaTnZANZBY8ZA69dixicjfAEw2cbpCaNBE8ZA37kblCpANOadZBtCm27FUSaZCbGMZCc89TmVHx6Xt34qNUZCP27olcX3GPlVZCdikt5TupoRZB488l3jIlS2DJfH63SSSdwZDZD');                                                                      
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
@@ -290,6 +218,4 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 );                                                                                                                   
 $result = curl_exec($ch);
 }
-
-
 ?>
