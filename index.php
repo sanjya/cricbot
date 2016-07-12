388d575a70a74193a498c4415dd9aae5ab6d069a
sanjya <sanjayanuwanaj@gmail.com>
2016-07-12 07:54:08 +0530

    New class

--- index.php
+++ index.php
@@ -178,8 +178,8 @@ $jsonData = '{
 
 function sendWelcomeMessage($recipientId){
     
-    global $access_token;
-    $input=  json_decode(file_get_contents("https://graph.facebook.com/v2.6/".$recipientId."?access_token=".$access_token,true);
+    //global $access_token;
+    $input=  json_decode(file_get_contents("https://graph.facebook.com/v2.6/".$recipientId."?access_token=https://www.codeofaninja.com/2015/12/angularjs-crud-example-php.html",true);
     $messageText="Hi ".$input['first_name'];
     sendTextMessage($recipientId, $messageText);
 	//print $messageText;
